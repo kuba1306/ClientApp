731cfbf3a36c1f8da34df9189e110cd81cc6ce21
kuba1306 <jakubpanienski@wp.pl>
2022-12-29 23:46:53 +0100

    first commit

---
+++ src/main/java/com/primesoftclient/PrimesoftClientApplication.java
@@ -0,0 +1,13 @@
+package com.primesoftclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimesoftClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrimesoftClientApplication.class, args);
+    }
+
+}
